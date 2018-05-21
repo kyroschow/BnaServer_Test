5dd319567e179bc8108624b528f98c8289e30ef4
TheCompSciNoob <kyroschow@gmail.com>
2018-05-21 14:31:30 -0700

    recreated JS functionality

--- bnanode/app.js
+++ bnanode/app.js
@@ -1,2 +1,5 @@
 kotlin = require('../out/production/BnaServer/lib/kotlin.js');
+app = require('express')();
+server = require('http').Server(app);
+io = require('socket.io')(server);
 require('../out/production/BnaServer/BnaServer.js');
--- src/BnaConfig.kt
+++ src/BnaConfig.kt
@@ -1,23 +1,42 @@
+import kotlin.js.Json
+import kotlin.js.json
+
+external val server: Server
+external val io: IO
+
 fun main(args: Array<String>) {
-    val app = require("../../../bnanode/node_modules/express")()
-    val server = require("http").Server(app)
-    val io = require("socket.io")(server)
+    server.listen(8080) {
+        console.log("Server is now running...")
+    }
+    io.on("connection") { socket ->
+        console.log("Player Connected!")
+        socket.emit("SocketID", json("id" to socket.id))
+        socket.broadcast.emit("NewPlayerJoined", json("id" to socket.id))
+        socket.on("disconnect") {
+            console.log("Player Disconnected")
+        }
+    }
 }
 
-external fun require(module: String): dynamic
+external class Server {
+    fun listen(port: Int, callback: () -> Unit)
+}
+
+external class IO {
+    fun on(event: String, callback: (Socket) -> Unit)
+}
+
+external class Socket {
+    fun on(event: String, callback: () -> Unit)
+
+    fun emit(name: String, info: Json)
+
+    var id: String
+
+    var broadcast: Socket
+}
 
 /*
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var tempPlayers = [];
-var players = [];
-var host;
-
-server.listen(8080, function() {
-    console.log("Server is now running...");
-});
-
 io.on('connection', function(socket) {
     console.log("Player connected!");
     socket.emit('SocketID', { id: socket.id })
